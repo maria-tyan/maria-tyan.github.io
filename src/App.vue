83adb00ed357dce7a040787c7a46650f96e5adfc
Maria Efremova <maria375797@gmail.com>
2020-09-26 01:30:23 +0300

    Add styles for header and for main section

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
   <div id="app">
     <header class="header">
-      <h1>
-        Maria Efremova
-      </h1>
-      <p>Front-end Developer</p>
-    </header>
-    <section class="container">
-      <div class="card">
-        <h1>
+      <div class="header__container">
+        <h3 class="header__title">
           Maria Efremova
-        </h1>
-        <p>Front-end Developer</p>
+        </h3>
+        <h4 class="header__subtitle">
+          / Front-end Developer
+        </h4>
+      </div>
+    </header>
+    <section class="section">
+      <div class="about">
+        <div class="about__card card">
+          <div class="card__photo" />
+          <h1 class="card__title">
+            Maria Efremova
+          </h1>
+          <p class="card__text">Front-end Developer</p>
+        </div>
+        <div class="about__info">
+          <h2>
+            Hello
+          </h2>
+          <h3>
+            Here is who I am and what I do
+          </h3>
+          <a href="/" class="button">
+            CV
+          </a>
+          <a href="/" class="button">
+            Projects
+          </a>
+          <p>
+            I'm FrontEnd Developer with 4+ years of experience
+            with modern JavaScript and with VueJS applications.
+          </p>
+          <p>
+            I have knowledge and experience working in Front-end as well as Back-end.
+          </p>
+          <p>
+            Also, I skilled in writing styles with pre-processors and methodologies
+            (Less, SCSS, Stylus and BEM methodology), having good understanding of UX and
+            accessibility and knowledge of FrontEnd toolset (WebPack, Gulp, Babel, etc.)
+          </p>
+          <p>
+            My experience includes work with cross-browser development,
+            build architectures for projects and enlivening sites with animations.
+          </p>
+        </div>
       </div>
     </section>
     <footer class="footer">
@@ -34,5 +71,9 @@ export default {
 
 <style lang="less">
   @import "./less/global.less";
+  @import "./less/header.less";
+  @import "./less/about.less";
+  @import "./less/card.less";
+  @import "./less/button.less";
   @import "./less/footer.less";
 </style>
