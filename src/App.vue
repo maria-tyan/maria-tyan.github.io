7d99375b7fae067fa4e15ba68bc753d26ea31beb
Maria Efremova <maria375797@gmail.com>
2020-09-27 01:39:17 +0300

    Add styles for main section and buttons

--- src/App.vue
+++ src/App.vue
@@ -19,35 +19,39 @@
           </h1>
           <p class="card__text">Front-end Developer</p>
         </div>
-        <div class="about__info">
-          <h2>
+        <div class="about__info info">
+          <h2 class="info__title">
             Hello
           </h2>
-          <h3>
+          <h3 class="info__subtitle">
             Here is who I am and what I do
           </h3>
-          <a href="/" class="button">
-            CV
-          </a>
-          <a href="/" class="button">
-            Projects
-          </a>
-          <p>
-            I'm FrontEnd Developer with 4+ years of experience
-            with modern JavaScript and with VueJS applications.
-          </p>
-          <p>
-            I have knowledge and experience working in Front-end as well as Back-end.
-          </p>
-          <p>
-            Also, I skilled in writing styles with pre-processors and methodologies
-            (Less, SCSS, Stylus and BEM methodology), having good understanding of UX and
-            accessibility and knowledge of FrontEnd toolset (WebPack, Gulp, Babel, etc.)
-          </p>
-          <p>
-            My experience includes work with cross-browser development,
-            build architectures for projects and enlivening sites with animations.
-          </p>
+          <div class="info__btn-wrapper">
+            <a href="/" class="button">
+              <span class="button__text">CV</span>
+            </a>
+            <a href="/" class="button">
+              <span class="button__text">Projects</span>
+            </a>
+          </div>
+          <div v-if=false>
+            <p>
+              I'm FrontEnd Developer with 4+ years of experience
+              with modern JavaScript and with VueJS applications.
+            </p>
+            <p>
+              I have knowledge and experience working in Front-end as well as Back-end.
+            </p>
+            <p>
+              Also, I skilled in writing styles with pre-processors and methodologies
+              (Less, SCSS, Stylus and BEM methodology), having good understanding of UX and
+              accessibility and knowledge of FrontEnd toolset (WebPack, Gulp, Babel, etc.)
+            </p>
+            <p>
+              My experience includes work with cross-browser development,
+              build architectures for projects and enlivening sites with animations.
+            </p>
+          </div>
         </div>
       </div>
     </section>
@@ -74,6 +78,7 @@ export default {
   @import "./less/header.less";
   @import "./less/about.less";
   @import "./less/card.less";
+  @import "./less/info.less";
   @import "./less/button.less";
   @import "./less/footer.less";
 </style>
